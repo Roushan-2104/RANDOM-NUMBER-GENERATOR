2232d3abd4ff8c081dc0fcbe54d80090559b5ef0
Roushan Sharma <87894817+Roushan-2104@users.noreply.github.com>
2021-09-04 10:52:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let number = document.getElementById("number");
 
-function generate() {
+let generate = document.getElementById("generate").addEventListener("click",function (){
     number.innerText = Math.round(Math.random() * 100);
-}
+});
